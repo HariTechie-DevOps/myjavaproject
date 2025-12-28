7ddbb7c504304feada7f0861e91464b4b53665bd
Harisankar <harisankar2206@gmail.com>
2025-12-28 19:17:23 +0530

    Update SignupApiController.java

--- src/main/java/com/spark/chat/controller/SignupApiController.java
+++ src/main/java/com/spark/chat/controller/SignupApiController.java
@@ -1,39 +1,23 @@
-package com.spark.chat.controller;
+package com.example.signup.controller;
 
-import com.example.signup.entity.User;
-import com.example.signup.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.signup.dto.SignupRequest;
+import com.example.signup.dto.SignupResponse;
+import com.example.signup.service.SignupService;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*") // Allows your browser to talk to the backend without security blocks
 public class SignupApiController {
 
-    @Autowired
-    private UserRepository userRepository;
-    
+    private final SignupService signupService;
+
+    public SignupApiController(SignupService signupService) {
+        this.signupService = signupService;
+    }
 
     @PostMapping("/signup")
-    public Map<String, Object> registerUser(@RequestBody User user) {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            User savedUser = userRepository.save(user);
-            response.put("success", true);
-            response.put("token", "session_token_12345");
-        
-        // Adding age and gender to the response
-            response.put("userName", savedUser.getName());
-            response.put("userAge", savedUser.getAge());
-            response.put("userGender", savedUser.getGender());
-        
-        } catch (Exception e) {
-            response.put("success", false);
-            response.put("message", "Registration failed!");
-        }
-        return response;
+    public SignupResponse processSignup(@RequestBody SignupRequest request) {
+        // Calls the service to check uniqueness and save to DB
+        return signupService.signup(request);
     }
 }
