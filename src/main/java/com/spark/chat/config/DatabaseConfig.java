c8d4bf613e8e0b61eda048eadae15ebce0713c2a
Harisankar <harisankar2206@gmail.com>
2025-12-28 19:23:25 +0530

    Create DatabaseConfig.java

---
+++ src/main/java/com/spark/chat/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.example.signup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        // Defining the connection inside the code
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/signup_app?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root"); // Replace with your MySQL password
+        return dataSource;
+    }
+}
