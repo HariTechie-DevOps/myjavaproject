dbb31b6e5aaa96d74e8ae5d98a7030ca7a477a5b
Harisankar <harisankar2206@gmail.com>
2025-12-28 19:10:28 +0530

    Update SignupRequest.java

--- src/main/java/com/spark/chat/dto/SignupRequest.java
+++ src/main/java/com/spark/chat/dto/SignupRequest.java
@@ -1,10 +1,9 @@
 package com.example.signup.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class SignupRequest {
     private String name;
     private int age;
