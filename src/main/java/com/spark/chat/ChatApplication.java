771fde9d4f9ca8d62266708fda1395b3659bb93e
Harisankar <harisankar2206@gmail.com>
2025-12-28 19:24:10 +0530

    Update ChatApplication.java

--- src/main/java/com/spark/chat/ChatApplication.java
+++ src/main/java/com/spark/chat/ChatApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import java.util.Collections;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.spark.chat", "com.example.signup"})
@@ -12,6 +13,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = "com.example.signup.entity")
 public class ChatApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ChatApplication.class, args);
+        SpringApplication app = new SpringApplication(ChatApplication.class);
+        
+        // Setting JPA properties programmatically instead of application.properties
+        app.setDefaultProperties(Collections.singletonMap("spring.jpa.hibernate.ddl-auto", "update"));
+        app.setDefaultProperties(Collections.singletonMap("spring.jpa.show-sql", "true"));
+        
+        app.run(args);
     }
 }
