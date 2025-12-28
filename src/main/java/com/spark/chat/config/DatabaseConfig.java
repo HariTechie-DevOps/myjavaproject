2e7e42b0990f936d16dcc8dfffc92495fe598fe0
Harisankar <harisankar2206@gmail.com>
2025-12-28 22:04:48 +0530

    Update DatabaseConfig.java

--- src/main/java/com/spark/chat/config/DatabaseConfig.java
+++ src/main/java/com/spark/chat/config/DatabaseConfig.java
@@ -1,4 +1,4 @@
-package com.example.signup.config;
+package com.spark.chat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
