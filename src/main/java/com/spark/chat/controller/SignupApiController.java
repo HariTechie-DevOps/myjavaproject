a366ebdffb3c1edc576f079a40a19814123ced8c
Harisankar <harisankar2206@gmail.com>
2025-12-29 09:52:17 +0530

    Update SignupApiController.java

--- src/main/java/com/spark/chat/controller/SignupApiController.java
+++ src/main/java/com/spark/chat/controller/SignupApiController.java
@@ -1,6 +1,5 @@
 package com.spark.chat.controller;
 
-
 import com.spark.chat.dto.SignupRequest;
 import com.spark.chat.dto.SignupResponse;
 import com.spark.chat.service.SignupService;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*") // Added: Important for frontend connection
 public class SignupApiController {
 
     private final SignupService signupService;
@@ -18,7 +18,7 @@ public class SignupApiController {
 
     @PostMapping("/signup")
     public SignupResponse processSignup(@RequestBody SignupRequest request) {
-        // Calls the service to check uniqueness and save to DB
+        // Calls the service method 'signup'
         return signupService.signup(request);
     }
 }
